6afee2911c59bc4496f292d494e9842fcebf7c92
Allie Shackelford <allieshackelford@gmail.com>
2023-11-16 13:53:26 -0800

    throwaway commit to trigger submodule update

--- Game/Source/Game.cpp
+++ Game/Source/Game.cpp
@@ -8,7 +8,7 @@ int main()
 {
 	const auto game = new Game();
 	const auto engine = std::make_unique<EngineContext>(game, LLGL::Extent2D{800, 600}, "Sandbox Game",
-	                                                    LLGL::ColorRGBAf{1.0f, 0.5f, 1.0f});
+	                                                    LLGL::ColorRGBAf{1.0f, 0.5f, 1.0f}, false);
 
 	engine->Run(game);
 
@@ -33,6 +33,11 @@ void Game::Update(float dt) const
 
 void Game::Render()
 {
+	mEngine->DrawText2D("Allie", {100, 100}, {2, 2}, {0.3f, 0.2f, 1.0f, 0.4f});
+	mEngine->DrawCircle({100, 100, 0}, 10.0f, {1.0f, 1.0f, 1.0f, 0.2f});
+	mEngine->DrawCircle({100, 150, 0}, 10.0f, {1.0f, 1.0f, 1.0f, 1.0f});
+
+	mEngine->DrawBox({200, 150, 0}, {10.0f, 100.0f, 1.0f}, {1.0f, 1.0f, 1.0f, 1.0f});
 }
 
 void Game::RegisterEntityDescriptions() const
