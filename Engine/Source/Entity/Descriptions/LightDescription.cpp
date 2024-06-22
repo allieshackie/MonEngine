edc7ad507f89997f5f04dd27176b8198e2e764e9
Allie Shackelford <allieshackelford@gmail.com>
2024-06-22 13:12:09 -0700

    add Light component and json description parsing

---
+++ Engine/Source/Entity/Descriptions/LightDescription.cpp
@@ -0,0 +1,16 @@
+#include "Entity/Components/LightComponent.h"
+
+#include "LightDescription.h"
+
+void LightDescription::ApplyToEntity(EntityId entity, EntityRegistry& entityRegistry)
+{
+	entityRegistry.AddComponent<LightComponent>(entity, mColor);
+}
+
+void LightDescription::ParseJSON(const nlohmann::json& json)
+{
+	assert(json.contains(COLOR_STRING));
+	auto& color = json[COLOR_STRING];
+	assert(color.size() == 3);
+	mColor = glm::vec3(color[0], color[1], color[2]);
+}
---
+++ Engine/Source/Entity/Descriptions/LightDescription.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <glm/vec3.hpp>
+
+#include "DescriptionBase.h"
+
+class LightDescription : public DescriptionBase
+{
+public:
+	LightDescription() = default;
+	// Copy constructor/assignment operator
+	LightDescription(const LightDescription& other) = default;
+	LightDescription& operator=(const LightDescription& other) = default;
+	// Move constructor/assignment operator
+	LightDescription(LightDescription&& other) noexcept = default;
+	LightDescription& operator=(LightDescription&& rhs) noexcept = default;
+
+	~LightDescription() override = default;
+
+	void ApplyToEntity(EntityId entity, EntityRegistry& entityRegistry) override;
+	void ParseJSON(const nlohmann::json& json) override;
+
+	static constexpr char JsonName[] = "light";
+
+private:
+	static constexpr char COLOR_STRING[] = "color";
+
+	glm::vec3 mColor = {0, 0, 0};
+};
