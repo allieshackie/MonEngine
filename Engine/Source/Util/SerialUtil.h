7e42fbbb4b5008e334d155e66748a77ae7c87d99
Allie Shackelford <allieshackelford@gmail.com>
2024-11-10 21:40:04 -0800

    add serializer helpers for glm types

---
+++ Engine/Source/Util/SerialUtil.h
@@ -0,0 +1,90 @@
+#pragma once
+#include <glm/vec3.hpp>
+#include <cereal/types/string.hpp>
+#include <cereal/types/vector.hpp>
+#include <cereal/archives/json.hpp>
+
+#include "Graphics/Animation/Animation.h"
+
+enum class ColliderShapes : int
+{
+	BOX = 0,
+	SPHERE,
+	CAPSULE,
+};
+
+inline std::map<std::string, ColliderShapes> COLLIDER_NAMES = {
+	{"Box", ColliderShapes::BOX},
+	{"Sphere", ColliderShapes::SPHERE},
+	{"Capsule", ColliderShapes::CAPSULE},
+};
+
+inline std::map<ColliderShapes, std::string> COLLIDER_STRINGS = {
+	{ColliderShapes::BOX, "Box"},
+	{ColliderShapes::SPHERE, "Sphere"},
+	{ColliderShapes::CAPSULE, "Capsule"},
+};
+
+namespace cereal
+{
+	template <class Archive>
+	void serialize(Archive& archive, glm::vec2& v) { archive(v.x, v.y); }
+
+	template <class Archive>
+	void serialize(Archive& archive, glm::vec3& v) { archive(v.x, v.y, v.z); }
+
+	template <class Archive>
+	void serialize(Archive& archive, glm::vec4& v) { archive(v.x, v.y, v.z, v.w); }
+
+	template <>
+	struct specialize<JSONInputArchive, AnimationStates, cereal::specialization::non_member_load_save>
+	{
+	};
+
+	template <>
+	struct specialize<JSONOutputArchive, AnimationStates, cereal::specialization::non_member_load_save>
+	{
+	};
+
+	// Save function for writing the enum as a string
+	template <class Archive>
+	void save(Archive& archive, const AnimationStates& state)
+	{
+		archive(ANIM_STRINGS[state]);
+	}
+
+	// Load function for reading a string and converting it to an enum
+	template <class Archive>
+	void load(Archive& archive, AnimationStates& state)
+	{
+		std::string stateStr;
+		archive(stateStr);
+		state = ANIM_NAMES[stateStr];
+	}
+
+	template <>
+	struct specialize<JSONInputArchive, ColliderShapes, cereal::specialization::non_member_load_save>
+	{
+	};
+
+	template <>
+	struct specialize<JSONOutputArchive, ColliderShapes, cereal::specialization::non_member_load_save>
+	{
+	};
+
+	// Save function for writing the enum as a string
+	template <class Archive>
+	void save(Archive& archive, const ColliderShapes& state)
+	{
+		archive(COLLIDER_STRINGS[state]);
+	}
+
+	// Load function for reading a string and converting it to an enum
+	template <class Archive>
+	void load(Archive& archive, ColliderShapes& state)
+	{
+		std::string stateStr;
+		archive(stateStr);
+		state = COLLIDER_NAMES[stateStr];
+	}
+} // namespace cereal
