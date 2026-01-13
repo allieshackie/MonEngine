b8edbc3aced73a4e22b4aa4c1d14b5736a7b70eb
Allie Shackelford <allieshackelford@gmail.com>
2026-01-12 23:19:21 -0800

    remove Game, since it's meant to be a completely separate proj

--- Game/Source/Entity/Descriptions/InteractDescription.cpp
+++
@@ -1,9 +0,0 @@
-#include "Entity/Components/InteractComponent.h"
-
-#include "InteractDescription.h"
-
-void InteractDescription::ApplyToEntity(Entity* entity, entt::registry& registry)
-{
-	InteractComponent interact;
-	entity->AddComponent<InteractComponent>(interact);
-}
--- Game/Source/Entity/Descriptions/OverlayDescription.cpp
+++
@@ -1,9 +0,0 @@
-#include "Entity/Components/OverlayComponent.h"
-
-#include "OverlayDescription.h"
-
-void OverlayDescription::ApplyToEntity(Entity* entity, entt::registry& registry)
-{
-	OverlayComponent overlay;
-	entity->AddComponent<OverlayComponent>(overlay);
-}
