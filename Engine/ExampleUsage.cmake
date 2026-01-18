4235decabdc53db7514e287aa5bf5cee8cb8398c
Allie Shackelford <allieshackelford@gmail.com>
2026-01-17 23:32:59 -0800

    last tweaks to dial in engine submodule set up

--- Engine/ExampleUsage.cmake
+++ Engine/ExampleUsage.cmake
@@ -1,38 +1,13 @@
 # Example of how to use MonEngine as a submodule in your project
 # This file shows different ways to integrate MonEngine
 
-# Method 1: Using add_subdirectory for the entire MonDev (includes Engine as submodule)
+# Using add_subdirectory for the entire MonDev (includes Engine as submodule)
 # Add this to your main CMakeLists.txt:
 #
 # add_subdirectory(path/to/MonDev)
 # target_link_libraries(YourTarget PRIVATE MonEngine)
-#
-# This includes the entire MonDev as a submodule but only builds the Engine library
 
-# Method 2: Using find_package (after installing MonEngine)
-# First install MonEngine:
-# cmake -B build -S path/to/MonEngine
-# cmake --build build --target install
-#
-# Then in your CMakeLists.txt:
-# find_package(MonEngine REQUIRED)
-# target_link_libraries(YourTarget PRIVATE MonEngine::MonEngine)
-
-# Method 3: Manual inclusion (not recommended)
-# target_include_directories(YourTarget PRIVATE path/to/MonEngine/Source)
-# target_link_libraries(YourTarget PRIVATE
-#     # List all MonEngine dependencies manually...
-# )
-
-# Required dependencies for your project:
-# - CMake 3.18.2 or higher
-# - vcpkg with the following packages:
-#   - entt
-#   - nlohmann-json
-#   - glm
-#   - lua
-#   - bullet3
-#
+# need to include a copy of vcpkg.json in main project root
 # Your project structure should look like:
 # YourProject/
 # ├── CMakeLists.txt
@@ -40,10 +15,7 @@
 # │   ├── CMakeLists.txt
 # │   ├── Source/
 # │   └── ...
-# ├── ThirdParty/         # As git submodule or copied
-# │   ├── imgui/
-# │   ├── LLGL/
-# │   ├── tinygltf/
-# │   └── vcpkg/
 # └── src/
 #     └── main.cpp
+# |
+# └── vcpkg.json <---- basically copy of file in MonDev but with a custom 'name'
